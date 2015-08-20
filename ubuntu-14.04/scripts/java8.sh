d763e42015f6f7ac36da4f35bd9016b6f16e44c1
Alex Kuiper <alexkuiper@bol.com>
2015-08-20 14:24:14 +0200

    Added sudo to java8
    Change-Id: Iff26e0c56e01f5009d2c8900ac21e72dbe958cfc

--- ubuntu-14.04/scripts/java8.sh
+++ ubuntu-14.04/scripts/java8.sh
@@ -7,4 +7,4 @@ sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
-DEBIAN_FRONTEND=noninteractive apt-get install -yq oracle-java8-installer
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -yq oracle-java8-installer
