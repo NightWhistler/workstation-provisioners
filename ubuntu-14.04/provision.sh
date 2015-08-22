26f2815a09674940a91b049aa0c624bdca34e60d
Alex Kuiper <alex@nightwhistler.net>
2015-08-22 12:15:31 -0400

    More fixes

--- ubuntu-14.04/provision.sh
+++ ubuntu-14.04/provision.sh
@@ -37,15 +37,14 @@ print_line "2. Install Packages ###############################################"
 # accept the ttf-mscorefonts-installer EULA ahead of time
 sudo -E debconf-set-selections <<< "ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true"
 
+# create commonly required directories
+cd ${HOME}
+mkdir -p "bin" ".icons" ".themes" "Apps" "Source"
+
 # run the package installation script
 #. ${scripts}/install.sh > /dev/null
 . ${scripts}/install.sh 
 cd ${current_directory}
-
-
-# create commonly required directories
-cd ${HOME}
-mkdir -p "bin" "Projects" ".icons" ".themes"
 
 # add bin to path
 echo 'export PATH=${PATH}:${HOME}/bin' | tee --append "${HOME}/.bashrc"
@@ -71,11 +70,13 @@ cp ~/.bashrc ~/.bashrc.backup
 echo 'export VISUAL="vim"' | tee --append "${HOME}/.bashrc"
 echo 'export EDITOR="${VISUAL}"' | tee --append "${HOME}/.bashrc"
 
+echo 'set -o vi' | tee --append "${HOME}/.bashrc"
+
 # Set a random hostname instead of 'vagrant-box'
-#export HOST=`rl -c1 hostnames.txt`
-#sudo hostname $HOST
-#sudo HOST=$HOST sh -c 'echo $HOST > /etc/hostname'
-#sudo sed -i s/vagrant-box/$HOST/ /etc/hosts 
+export HOST=`rl -c1 hostnames.txt`
+sudo hostname $HOST
+sudo HOST=$HOST sh -c 'echo $HOST > /etc/hostname'
+sudo sed -i s/vagrant-box/$HOST/ /etc/hosts 
 
 print_line "3. Install Ubuntu Privacy Fixes ###################################"
 # execute the fix ubuntu script to ensure additional privacy
