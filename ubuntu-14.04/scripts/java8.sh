e8e6ff34277a280942f33d95ecb0416235c7ead9
Alex Kuiper <alex@nightwhistler.net>
2015-08-23 17:27:31 +0200

    Updated java 8 script

--- ubuntu-14.04/scripts/java8.sh
+++ ubuntu-14.04/scripts/java8.sh
@@ -6,5 +6,5 @@ echo "Installing Package: Java 8"
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
-DEBIAN_FRONTEND=noninteractive sudo apt-get install -yq oracle-java8-installer
+sudo sh -c 'echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections'
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -yq oracle-java8-installer
