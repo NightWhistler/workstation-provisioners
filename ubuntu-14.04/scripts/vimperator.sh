b166b4388ab33aa432052c32454e535f45f4215f
Alex Kuiper <alexkuiper@bol.com>
2015-08-24 20:21:55 +0200

    Added missing script
    Change-Id: I2dcce22051fb38edba5775b2ae4c4d04a1b0135c

---
+++ ubuntu-14.04/scripts/vimperator.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Vimperator
+echo "Installing Package: vimperator"
+
+sudo ../helpers/install-mozilla-addon.sh https://addons.mozilla.org/firefox/downloads/file/298890/vimperator-3.9-fx.xpi
