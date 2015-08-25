8f92046ae897ea0ab4c5a379b1f24e4b7abdf000
Alex Kuiper <alex@nightwhistler.net>
2015-08-25 05:29:47 +0000

    Slightly more advanced way of relative folder

--- ubuntu-14.04/scripts/vimperator.sh
+++ ubuntu-14.04/scripts/vimperator.sh
@@ -3,4 +3,5 @@
 # Vimperator
 echo "Installing Package: vimperator"
 
-sudo ../helpers/install-mozilla-addon.sh https://addons.mozilla.org/firefox/downloads/file/298890/vimperator-3.9-fx.xpi
+export FOLDER=`dirname $0`
+sudo $FOLDER/../helpers/install-mozilla-addon.sh https://addons.mozilla.org/firefox/downloads/file/298890/vimperator-3.9-fx.xpi
