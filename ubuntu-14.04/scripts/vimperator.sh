2a7dbd2dc71f4ec6877236400692c234bfaeb994
Alex Kuiper <alex@nightwhistler.net>
2015-09-05 09:26:08 +0200

    Better way of determing current dir

--- ubuntu-14.04/scripts/vimperator.sh
+++ ubuntu-14.04/scripts/vimperator.sh
@@ -3,5 +3,5 @@
 # Vimperator
 echo "Installing Package: vimperator"
 
-export FOLDER=`dirname $0`
-sudo $FOLDER/../helpers/install-mozilla-addon.sh https://addons.mozilla.org/firefox/downloads/file/298890/vimperator-3.9-fx.xpi
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+sudo $DIR/../helpers/install-mozilla-addon.sh https://addons.mozilla.org/firefox/downloads/file/298890/vimperator-3.9-fx.xpi
