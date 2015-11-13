948aae49f0ee9460532dd6fc20f5cc823b55121d
Alex Kuiper <alex@nightwhistler.net>
2015-11-13 08:47:56 +0100

    Added gnome settings daemon

---
+++ ubuntu-14.04/scripts/i3.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+# i3
+echo "Installing Package: i3"
+
+# add ppa repository
+sudo -E add-apt-repository -y ppa:aacebedo/j4-dmenu-desktop > /dev/null
+
+# update packages repository
+sudo -E apt-get update -qq > /dev/null
+
+# install the package
+sudo -E apt-get install -qq i3 j4-dmenu-desktop python-setuptools udisks python-pip python-gobject python-yaml libgio2.0 gobject-introspection libgtk2.0-0 libnotify4 gettext gir1.2-notify-0.7 gnome-settings-daemon
+
+sudo pip install udiskie
