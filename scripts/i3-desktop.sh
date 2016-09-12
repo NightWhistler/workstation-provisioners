095a256f63b956bb687c90bc78a35b4800088664
Alex Kuiper <alex@nightwhistler.net>
2016-09-12 11:29:51 +0200

    Updated i3 desktop

--- scripts/i3-desktop.sh
+++ scripts/i3-desktop.sh
@@ -11,6 +11,6 @@ echo "Installing Package: i3"
 sudo -E apt-get update -qq > /dev/null
 
 # install the package
-sudo -E apt-get install -qq i3 j4-dmenu-desktop suckless-tools python-setuptools python-pip python-gobject python-yaml libgio2.0 gobject-introspection libgtk2.0-0 libnotify4 gettext gir1.2-notify-0.7 gnome-settings-daemon feh ranger
+sudo -E apt-get install -qq i3 j4-dmenu-desktop suckless-tools python-setuptools python-pip python-gobject python-yaml libgio2.0 gobject-introspection libgtk2.0-0 libnotify4 gettext gir1.2-notify-0.7 gnome-settings-daemon feh ranger compton
 
 sudo pip install udiskie
