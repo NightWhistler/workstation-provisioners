2bfd4da02dc7369656f0e8f52a86b33a47ad55ae
Alex Kuiper <alex@nightwhistler.net>
2016-09-16 23:54:07 +0200

    Notifications

--- scripts/i3-desktop.sh
+++ scripts/i3-desktop.sh
@@ -14,3 +14,6 @@ sudo -E apt-get update -qq > /dev/null
 sudo -E apt-get install -qq i3 j4-dmenu-desktop suckless-tools python-setuptools python-pip python-gobject python-yaml libgio2.0 gobject-introspection libgtk2.0-0 libnotify4 gettext gir1.2-notify-0.7 gnome-settings-daemon feh ranger compton rofi
 
 sudo pip install udiskie
+
+#Remove the 'dunst' daemon since it causes ugly notifications
+apt-get remove dunst
